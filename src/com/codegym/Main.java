1745269a8a00ba7e1875eee1d419cec7ae8fddd7
Toan-246 <n.v.toan687@gmail.com>
2022-01-10 19:28:58 +0700

    add

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,9 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Student student1 = new Student();
+        System.out.println(student1.toString());
+    }
+}
